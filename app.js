411948345ed8e2992ae4bdf2620966b9f1cad639
jamesattoh <koss.attoh27@gmail.com>
2025-02-28 00:24:20 +0100

    GET route creation

--- app.js
+++ app.js
@@ -2,26 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log('Requete recue !');
+//configuration des en-têtes CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //Permet les requêtes provenant de n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //Permet certains en-têtes spécifiques dans les requêtes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //Permet certaines méthodes HTTP spécifiques
     next();
-})
+  });
 
-app.use((req, res, next)=> {
-    res.status(201);
-    next();
-})
-
-//le moyen de reponse de notre app
-app.use((req, res, next)=> {
-    res.json({ message : 'Hein! Votre requete est bien reçue'});
-    next();
-})
-
-app.use((req, res)=>{
-    console.log('Reponse envoyée avec succès !')
-})
-
-
+// rendre disponible une liste de certains objets à travers l'api
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
