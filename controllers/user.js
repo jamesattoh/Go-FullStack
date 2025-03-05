99f427ed09768c73ef8590beec8ca696276c5b4d
jamesattoh <koss.attoh27@gmail.com>
2025-03-06 00:03:00 +0100

    users creation

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,21 @@
-exports.signup = (req, res, next) =>{
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
+exports.signup = (req, res, next) =>{
+    // on doit premierement hasher le password car c'est une operation asynchrone qui est un peu chronophage et dans laquelle on recoit le hash généré
+    bcrypt.hash(req.body.password, 10) // on crypte le mot de passe
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+                .catch(error => res.status(400).json({ error }))
+        })
+        .catch( error => res.status(500).json({ error })) //500 pour l'erreur serveur
 }
 
 exports.login = (req, res, next) =>{
-    
+
 }
